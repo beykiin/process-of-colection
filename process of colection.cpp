0803659833746cc0152186b652c7bfffecd7a500
beykiin <132366582+beykiin@users.noreply.github.com>
2024-02-18 14:23:18 +0300

    Add files via upload

---
+++ process of colection.cpp
@@ -0,0 +1,35 @@
+﻿// process of colection.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2, total;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    total = num1 + num2;
+
+    cout << "Total: " << total << endl;
+
+    return 0;
+
+}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
